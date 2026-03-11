a669c8a1a28945ef0f050f594a2413ec136d2bf6
pablonv2005 <pablonv2005@gmail.com>
2026-03-11 11:49:59 +0100

    Script
    Hago el script

--- textoImagen.js
+++ textoImagen.js
@@ -1,2 +1,16 @@
 const imagenes= document.querySelectorAll(".ImagenTexto");
 const texto= document.getElementById("textoHover");
+
+imagenes.forEach(imagen=> {
+   imagen.addEventListener("mouseenter", ()=> {
+    texto.style.display= "block";
+   });
+   imagen.addEventListener("mouseleave", ()=> {
+    texto.style.display= "none";
+   });
+   imagen.addEventListener("mousemove", (e)=> {
+    texto.style.left= e.pageX + "px";
+        texto.style.top= e.pageY + "px";
+
+   });
+});
